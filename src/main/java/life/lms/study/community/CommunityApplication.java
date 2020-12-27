4688da350798928abb31dc2ef01da866b5f0d314
Administrator <1977267068@qq.com>
2020-12-27 15:08:28 +0800

    init repo

---
+++ src/main/java/life/lms/study/community/CommunityApplication.java
@@ -0,0 +1,16 @@
+package life.lms.study.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lms
+ */
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
---
+++ src/main/java/life/lms/study/community/controller/HelloController.java
@@ -0,0 +1,22 @@
+package life.lms.study.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author shkstart
+ * @create 2020-12-27 14:17
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+
+    }
+
+}
